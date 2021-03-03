39decfad54929ba9a680bd9f24d6e189b7245db9
pneumahub <39502544+pneumahub@users.noreply.github.com>
2021-03-03 16:58:40 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,22 +5,16 @@ local lib = {}
 
 --functions
 function lib.newUUID(seed)
-	math.randomseed(seed or tick());
-	local random = math.random
-	local template ='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
-	return string.gsub(template, '[xy]', function (c)
-		local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
-		return string.format('%x', v)
-	end);
+	return HttpService = game:GetService("HttpService"):GenerateGUID(false);
 end
 
 function lib.typeof(arg)
-	local mt = getmetatable(arg)
+	local mt = getmetatable(arg);
 
 	if mt and mt.__type then
-		return mt.__type
+		return mt.__type;
 	end
-	return typeof(arg)
+	return typeof(arg);
 end
 
 function lib.require_once(path)
